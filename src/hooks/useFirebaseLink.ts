64ac5a23fd0de8210bc712a07c0240beef66d8d6
polnikale <nikpolale@gmail.com>
2021-02-05 00:16:49 +0200

    feat: add try-catch to wrap async

--- src/hooks/useFirebaseLink.ts
+++ src/hooks/useFirebaseLink.ts
@@ -1,4 +1,4 @@
-import {useCallback, useEffect} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import useSignIn from './useSignIn';
 import dynamicLinks, {
   FirebaseDynamicLinksTypes,
@@ -6,14 +6,21 @@ import dynamicLinks, {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useFirebaseLink = (): [boolean, boolean] => {
-  const [onSignIn, ...signInInfo] = useSignIn();
+  const [onSignIn, isLoading, isError] = useSignIn();
+
+  const [isSignInError, setSignInError] = useState(false);
 
   const handleDynamicLink = useCallback<
     (link: FirebaseDynamicLinksTypes.DynamicLink) => void
   >(
     async (link) => {
-      const email = await AsyncStorage.getItem('email');
-      onSignIn(link.url, email);
+      try {
+        const email = await AsyncStorage.getItem('email');
+        onSignIn(link.url, email);
+      } catch (error) {
+        console.error('Error while signing user in:', error);
+        setSignInError(true);
+      }
     },
     [onSignIn],
   );
@@ -48,7 +55,7 @@ const useFirebaseLink = (): [boolean, boolean] => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return signInInfo;
+  return [isLoading, isError || isSignInError];
 };
 
 export default useFirebaseLink;
